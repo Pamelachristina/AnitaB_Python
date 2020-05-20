e7efdb857110731ca6226ed4039487a222639f05
Fuzzy Hair <pamelasanchezhernandez@pamelas-mbp.attlocal.net>
2020-05-19 20:38:32 -0400

    added spaces

--- python_sandbox_starter/Udemy-Python-assign2.py
+++ python_sandbox_starter/Udemy-Python-assign2.py
@@ -1,17 +1,30 @@
 # Create list
 countries = ['United States', 'Canada', 'Mexico']
+
+
 # Add country at the end
 countries.append('Ireland')
+
+
 # Remove by index
 del(countries[3])
+
+
 # Add a country in the middle
 countries.insert(2, 'United Kingdom',)
 countries.insert(3, 'Mali',)
+
 print(countries)
+
+
 # Create set
 country_set = {'United States', 'Canada', 'Mexico'}
+
+
 # Add country
 country_set.add('Ireland')
+
+
 # Remove
 country_set.remove('Canada')
 print(country_set)
